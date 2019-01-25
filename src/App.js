8c55ecd0660c702539b0db4de5f66e0bde9eb529
gargansa <gargansa@hotmail.com>
2019-01-25 15:15:18 -0700

    transform

--- src/App.js
+++ src/App.js
@@ -4,11 +4,43 @@ import Character from './Character.js';
 import Branch from './Branch.js';
 
 class App extends Component {
+  state = {
+    x: 0,
+    y: 0,
+    width: 0,
+    height: 0,
+  }
+  constructor() {
+    super()
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+    console.log("in app componentdidmount")
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions() {
+    this.setState({ x: Math.floor(window.innerWidth / 2), y: Math.floor(window.innerHeight * 0.8), width: window.innerWidth, height: window.innerHeight });
+  }
+
+
   render() {
+    var appStyle = {
+      position: 'relative',
+    }
     return (
-      <div className="App">
-      <Character />
-      <Branch x={100} y={100}/>
+      <div className="App" 
+      style={appStyle}
+      >
+        <Character />
+        <Branch x={this.state.x-100} y={this.state.y} z={0} />
+        <Branch x={this.state.x+100} y={this.state.y} z={1} />
       </div>
     );
   }
--- src/Branch.js
+++ src/Branch.js
@@ -6,19 +6,31 @@ class Branch extends Component {
     super(props)
     this.state={
       x:this.props.x,
-      y:this.props.x
+      y:this.props.y,
+      z:this.props.z
     }
+  }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.x !== this.state.x) {
+      this.setState({ x: nextProps.x});
+    }
+    if (nextProps.y !== this.state.y){
+      this.setState({ y: nextProps.y})
+    }
+
   }
 
   render() {
     var branchStyle ={
-      backgroundColor:'blue',
+      border: '1px dotted blue',
       position:'absolute',
-      transform: `translate(${this.state.x}px,${this.state.y}px)`,
+      transform: `translate(${this.state.x}px,${this.state.y}px) rotate(90deg)` ,
+      transformOrigin: `50px 50px`,
+      zIndex: this.state.z
   }
     return (
-      <div>
-        <div style={branchStyle}>branch</div>
+      <div style={branchStyle}>
+     <p>branch {this.state.x} {this.state.y}</p>
       </div>
     );
   }
